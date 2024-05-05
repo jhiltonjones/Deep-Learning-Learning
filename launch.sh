0daf971d173410d232ef5d4eea798e7f9b494f9a
jhiltonjones <jack.hiltonjones@gmail.com>
2024-05-05 14:49:15 +0100

    clean up

---
+++ launch.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -l
+#SBATCH -p ecsstaff
+#SBATCH --account=ecsstaff
+#SBATCH --partition=a100
+#SBATCH -c 48
+#SBATCH --nodes=1
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=jhj1g23@soton.ac.uk
+#SBATCH --time=06:00:00
+source /local/software/conda/miniconda-py3-new/etc/profile.d/conda.sh
+conda activate challenge
+python run_ac_offline.py --seed 1 --env_name Walker2d --dataset medium --discrete_control 0 --state_dim 17 --action_dim 6 --tau 0.33 --learning_rate 0.0003 --hidden_units 256 --batch_size 256 --timeout 1000 --max_steps 1000000 --log_interval 10000
